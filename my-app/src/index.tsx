38fcf47834caa1194b6aaa00bc8643e945a60f75
xomrau <hle3524@gmail.com>
2018-09-12 21:59:46 +1200

    render points to AppRouter

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import {AppRouter} from './router';
+
+// this file is where the final render is
+// render now points to AppRouter
 
 ReactDOM.render(
-  <App />,
+//  <App />,
+  <AppRouter />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
