de20b42422e8330bbd8c91adba4fdcd45d5ef31a
xomrau <hle3524@gmail.com>
2018-09-12 21:57:57 +1200

    remove default, add new render

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,22 +1,15 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
-
-class App extends React.Component {
+export default class App extends React.Component<{}> {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div className="container-fluid">
+      <div className="centreText">
+        {/* React components must have a wrapper node/element */}
+        <h1>( ͡° ͜ʖ ͡°)</h1>
       </div>
+    </div>
     );
-  }
-}
-
-export default App;
+  };
+};
