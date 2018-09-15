fbedddde01ee23c1348586e0cf130637d401f61b
xomrau <hle3524@gmail.com>
2018-09-16 00:25:54 +1200

    change to app tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,56 @@
 import * as React from 'react';
 import './App.css';
+// import 'api.json';
 
-export default class App extends React.Component<{}> {
+interface IState {
+  cuisines: any[],
+  foods: any[],
+  
+};
+
+// const apiKey = '';
+const cuisine = 'https://developers.zomato.com/api/v2.1/cuisines?city_id=70';
+const token = 'a1f83fbe9a478384319cb5f424ff097f';
+
+export default class App extends React.Component<{}, IState> {
+  constructor(props: any){
+    super(props)
+    this.state = {
+      cuisines: [],
+      foods: [],
+      
+    }
+  };
+
+  public getCuisine(){
+    fetch(cuisine,{
+      headers: {
+        'Accept': 'application/json',
+        'user-key': token,
+      },
+      method: 'GET',
+      
+    })
+    .then((response: any) => {
+      if (response.ok) {
+        return response.json();
+      } else{
+        throw new Error('Error!');
+      }
+    })
+    .then(data => this.setState({cuisines: data.cuisines_name}))
+  };
+  
+  public search(){
+    fetch('')
+  };
+  
   public render() {
     return (
       <div className="container-fluid">
       <div className="centreText">
         {/* React components must have a wrapper node/element */}
-        <h1>( ͡° ͜ʖ ͡°)</h1>
+        <div/>
       </div>
     </div>
     );
